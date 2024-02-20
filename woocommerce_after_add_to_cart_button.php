bace4bf54f975d6e30fb8834711f2dbbff6bed5a
imran0dot <imran0dot@gmail.com>
2024-02-20 10:16:51 +0600

    update html

--- woocommerce_after_add_to_cart_button.php
+++ woocommerce_after_add_to_cart_button.php
@@ -1,8 +1,8 @@
 <?php
 function add_extra_html_after_add_to_cart_button() {
     echo '<div class="extra-html">';
-    echo '<a class="button" href="https://wa.me/+8801990129353" style="display:inline-block; background-color:blue; color:white; padding:10px 15px; text-decoration:none; margin-right:10px;">What\'s app এ ওর্ডার করুন</a>';
-    echo '<a class="button" href="tel:+8801990129353" style="display:inline-block; background-color:green; color:white; padding:10px 15px; text-decoration:none;">Call Now</a>';
+    echo '<a class="button" href="https://wa.me/+8801*****" style="display:inline-block; background-color:blue; color:white; padding:10px 15px; text-decoration:none; margin-right:10px;">What\'s app এ ওর্ডার করুন</a>';
+    echo '<a class="button" href="tel:+8801*****" style="display:inline-block; background-color:green; color:white; padding:10px 15px; text-decoration:none;">Call Now</a>';
     echo '</div>';
 }
 add_action( 'woocommerce_after_add_to_cart_button', 'add_extra_html_after_add_to_cart_button' );
